cd6431b7c01cf4ea00ba57535042d154a08efe74
unknown <mostafa.algohary@gmail.com>
2021-01-23 13:43:44 +0200

    ss

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 // ----------------------------------------------------------------------------
 // Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 // ----------------------------------------------------------------------------
-
-use strict
-
 // This is a base-level Azure Mobile App SDK.
 var express = require('express'),
     azureMobileApps = require('azure-mobile-apps');

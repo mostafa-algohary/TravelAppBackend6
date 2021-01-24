c9ad8347bb2d9afaecb0b8b000b8ae94e6c8df17
unknown <mostafa.algohary@gmail.com>
2021-01-24 08:46:52 +0200

    sw22

--- tables/post.js
+++ tables/post.js
@@ -2,7 +2,7 @@ var table = module.exports = require('azure-mobile-apps').table();
 
 table.read(function (context) {
     return context.execute();
-    //return "sdsadas";
+    
 });
 
 table.insert(function (context) {

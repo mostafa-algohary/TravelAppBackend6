40b592382969894a39fcbb950d02118a672e2b58
unknown <mostafa.algohary@gmail.com>
2021-01-25 12:58:18 +0200

    sw22

--- server.js
+++ server.js
@@ -15,12 +15,4 @@ mobile.tables.initialize()
        });
 
 
-/// var app = require('express')(); // Create an instance of an Express app
-
-// var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
-
 // mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings
-
-// app.use(mobileApp);
-// app.listen(process.env.PORT || 3000);
-// console.log('Server JS App listening to :'  + process.env.PORT);

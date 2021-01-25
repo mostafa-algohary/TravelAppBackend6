1c4760c3e937b4b4465f3a65ea86b913e6b24f7b
unknown <mostafa.algohary@gmail.com>
2021-01-25 13:07:54 +0200

    sw22222

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ mobile = azureMobileApps();
 // Import the files from the tables directory to configure the /tables endpoint
 //mobile.tables.import('./tables');
 mobile.tables.add('Post');
-mobile.tables.initialize()
-    .then(function () {
-        app.use(mobile);    // Register the Azure Mobile Apps middleware
-        app.listen(process.env.PORT || 3000);   // Listen for requests
-       });
+app.use(mobile);    // Register the Azure Mobile Apps middleware
+app.listen(process.env.PORT || 3000);   // Listen for requests
+
+
+// mobile.tables.initialize()
+//     .then(function () {
+//         app.use(mobile);    // Register the Azure Mobile Apps middleware
+//         app.listen(process.env.PORT || 3000);   // Listen for requests
+//        });
        
 // mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings
